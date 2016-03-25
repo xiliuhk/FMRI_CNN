a98717caca130276ecef9b2092d5bd3c1eacd860
xiliuhk <xiliuhk@gmail.com>
2016-03-24 23:16:14 -0400

    tmp

--- tflow.py
+++ tflow.py
@@ -1,1 +1,28 @@
 __author__ = 'laceyliu'
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.001
+training_iters = 200000
+batch_size = 64
+display_step = 20
+
+# Network Parameters
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+dropout = 0.8 # Dropout, probability to keep units
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
+
+
+def conv2d(name, l_input, w, b):
+    return tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(l_input, w, strides=[1, 1, 1, 1], padding='SAME'),b), name=name)
+
+def max_pool(name, l_input, k):
+    return tf.nn.max_pool(l_input, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
+
+def norm(name, l_input, lsize=4):
+    return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001 / 9.0, beta=0.75, name=name)
